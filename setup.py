ba414ae679f20caa42d657fd423f7ea329bfa1ec
Mark Spicer <spicer93@gmail.com>
2015-08-27 14:11:36 -0400

    fixup.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 # Roastero, released under GPLv3
 
+import os
 from setuptools import setup
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+with open(os.path.join(here, 'requirements.txt')) as f:
+    requires = f.read().splitlines()
 
 setup(
     name='freshroastsr700',
@@ -13,4 +20,5 @@ setup(
     author='Roastero',
     author_email='admin@roastero.com',
     license='GPLv3',
-    packages=['freshroastsr700'])
+    packages=['freshroastsr700'],
+    install_requires=requires)
