7316778b03978fdaf0a396ee64647516c88280d6
Mark Spicer <spicer93@gmail.com>
2015-11-21 16:09:59 -0500

    Updated setup file to include long description and find_packages.

--- setup.py
+++ setup.py
@@ -3,22 +3,25 @@
 
 import os
 from setuptools import setup
+from setuptools import find_packages
 
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
+    long_description = f.read()
 with open(os.path.join(here, 'requirements.txt')) as f:
     requires = f.read().splitlines()
+
+description = 'A module for interfacing with a FreshRoastSR700 coffee roaster.'
 
 setup(
     name='freshroastsr700',
     version='0.2',
-    description=
-        'A module for interfacing with a FreshRoastSR700 coffee roaster.',
+    description=description,
+    long_description=long_description,
     url='https://github.com/Roastero/freshroastsr700',
     author='Roastero',
     author_email='admin@roastero.com',
     license='GPLv3',
-    packages=['freshroastsr700'],
+    packages=find_packages(),
     install_requires=requires)
