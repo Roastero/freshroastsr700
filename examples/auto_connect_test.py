561fd35395f4bd482aba32848295e10329da17ac
Alan@int3ll3ct <int3ll3ct.ly@gmail.com>
2017-05-22 11:08:15 -0400

    tabs issues, converted to spaces.

--- examples/auto_connect_test.py
+++ examples/auto_connect_test.py
@@ -3,62 +3,62 @@ import time
 import multiprocessing as mp
 
 if __name__ == "__main__":
-	mp.freeze_support()
+    mp.freeze_support()
 
-	roaster = freshroastsr700.freshroastsr700()
+    roaster = freshroastsr700.freshroastsr700()
 
-	# test auto-connect...
-	roaster.auto_connect()
+    # test auto-connect...
+    roaster.auto_connect()
 
-	wait_timeout = time.time() + 40.0
-	while not roaster.connected:
-		if time.time() > wait_timeout:
-			print("Waited for a connection, didn't find one, bailing.")
-			exit()
-		print("Connection state is %s" % str(roaster.connect_state))
-		time.sleep(0.5)
+    wait_timeout = time.time() + 40.0
+    while not roaster.connected:
+        if time.time() > wait_timeout:
+            print("Waited for a connection, didn't find one, bailing.")
+            exit()
+        print("Connection state is %s" % str(roaster.connect_state))
+        time.sleep(0.5)
 
-	roaster.fan_speed = 3
-	roaster.heat_setting = 1
-	roaster.time_remaining = 10
-	roaster.roast()
+    roaster.fan_speed = 3
+    roaster.heat_setting = 1
+    roaster.time_remaining = 10
+    roaster.roast()
 
-	time.sleep(6.0)
+    time.sleep(6.0)
 
-	roaster.fan_speed = 5
-	roaster.heat_setting = 0
-	roaster.time_remaining = 10
-	roaster.cool()
+    roaster.fan_speed = 5
+    roaster.heat_setting = 0
+    roaster.time_remaining = 10
+    roaster.cool()
 
-	time.sleep(6.0)
+    time.sleep(6.0)
 
-	roaster.idle()
+    roaster.idle()
 
-	time.sleep(0.5)
+    time.sleep(0.5)
 
-	roaster.disconnect()
+    roaster.disconnect()
 
-	time.sleep(2.0)
+    time.sleep(2.0)
 
-	# test a re-connect after a successful session
-	roaster.connect()
+    # test a re-connect after a successful session
+    roaster.connect()
 
-	roaster.fan_speed = 7
-	roaster.heat_setting = 1
-	roaster.time_remaining = 10
-	roaster.roast()
+    roaster.fan_speed = 7
+    roaster.heat_setting = 1
+    roaster.time_remaining = 10
+    roaster.roast()
 
-	time.sleep(6.0)
+    time.sleep(6.0)
 
-	roaster.fan_speed = 9
-	roaster.heat_setting = 0
-	roaster.time_remaining = 10
-	roaster.cool()
+    roaster.fan_speed = 9
+    roaster.heat_setting = 0
+    roaster.time_remaining = 10
+    roaster.cool()
 
-	time.sleep(6.0)
+    time.sleep(6.0)
 
-	roaster.idle()
+    roaster.idle()
 
-	time.sleep(0.5)
+    time.sleep(0.5)
 
-	roaster.disconnect()
+    roaster.disconnect()
